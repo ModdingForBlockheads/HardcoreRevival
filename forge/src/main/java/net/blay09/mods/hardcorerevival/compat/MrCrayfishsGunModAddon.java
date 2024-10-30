31c9071ee610a826d60c3ba43d874f1f46da3943
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-10-30 18:06:48 +0100

    fix: Fix compile error on Forge

--- forge/src/main/java/net/blay09/mods/hardcorerevival/compat/MrCrayfishsGunModAddon.java
+++ forge/src/main/java/net/blay09/mods/hardcorerevival/compat/MrCrayfishsGunModAddon.java
@@ -2,7 +2,7 @@ package net.blay09.mods.hardcorerevival.compat;
 
 import com.mrcrayfish.guns.event.GunFireEvent;
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.hardcorerevival.HardcoreRevival;
+import net.blay09.mods.hardcorerevival.PlayerHardcoreRevivalManager;
 import net.blay09.mods.hardcorerevival.config.HardcoreRevivalConfig;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
@@ -19,7 +19,7 @@ public class MrCrayfishsGunModAddon {
 
     @SubscribeEvent
     public void onGunFirePre(GunFireEvent.Pre event) {
-        if (HardcoreRevival.getRevivalData(event.getEntity()).isKnockedOut()) {
+        if (PlayerHardcoreRevivalManager.isKnockedOut(event.getEntity())) {
             ResourceLocation mainHandItemKey = Balm.getRegistries().getKey(event.getEntity().getMainHandItem().getItem());
             boolean isFiringPistol = Objects.equals(mainHandItemKey, PISTOL);
             if (isFiringPistol && HardcoreRevivalConfig.getActive().allowPistols) {
