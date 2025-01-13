fa59a0d6b0093c2434bc1099d2a162036d54c0e8
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:41:32 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/hardcorerevival/ForgeHardcoreRevival.java
+++ forge/src/main/java/net/blay09/mods/hardcorerevival/ForgeHardcoreRevival.java
@@ -1,19 +1,22 @@
 package net.blay09.mods.hardcorerevival;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.hardcorerevival.client.HardcoreRevivalClient;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(HardcoreRevival.MOD_ID)
 public class ForgeHardcoreRevival {
 
-    public ForgeHardcoreRevival() {
-        Balm.initialize(HardcoreRevival.MOD_ID, EmptyLoadContext.INSTANCE, HardcoreRevival::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(HardcoreRevival.MOD_ID, EmptyLoadContext.INSTANCE, HardcoreRevivalClient::initialize));
+    public ForgeHardcoreRevival(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(HardcoreRevival.MOD_ID, loadContext, HardcoreRevival::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(HardcoreRevival.MOD_ID, loadContext, HardcoreRevivalClient::initialize);
+        }
     }
 
 }
